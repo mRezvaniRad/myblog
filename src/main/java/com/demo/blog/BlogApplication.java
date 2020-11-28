83bd8063874ed2f683c4e7300cde7696e6240e29
marjan <m.rezvani.rad@gmail.com>
2020-11-28 22:38:21 +0330

    add project to repository

---
+++ src/main/java/com/demo/blog/BlogApplication.java
@@ -0,0 +1,14 @@
+package com.demo.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        System.out.println("marjan");
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
